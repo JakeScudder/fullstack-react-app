aa8520404859c2a2f801a8159a04789af2636b99
Jake M <scudsterr@gmail.com>
2020-01-13 17:48:23 -0500

    added node modules
    Got courses to display from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header> Hey </header>
+class App extends Component {
+
+  constructor() {
+  super();
+    this.state = {
+      courses: []
+    };
+  }
+
+  componentDidMount() {
+    this.handleFetch();
+  }
+
+  handleFetch = () => {
+    axios.get(`http://localhost:5000/api/courses`)
+      .then(res => {
+        this.setState({
+          courses: res.data.courses
+        }) 
+      })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error)
+    })
+  }
+
+  render() {
+    return (
+    <div>
+      {
+        this.state.courses.map(course => {
+          return (
+            <div>
+            <ul>
+              <h1> {course.title} </h1>
+              <p> {course.description} </p>
+            </ul>
+            
+          </div>
+          ) 
+        })
+      }
     </div>
-  );
+    )
+  }
 }
 
 export default App;
